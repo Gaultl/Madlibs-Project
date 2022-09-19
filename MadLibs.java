6785ec2344d0aeb1e734ac0d5540e4a21f56756f
Gaultl <113708486+Gaultl@users.noreply.github.com>
2022-09-19 08:57:06 -0700

    Update MadLibs.java

--- MadLibs.java
+++ MadLibs.java
@@ -55,10 +55,10 @@ public class MadLibs
         System.out.println(name + "     " + month + "/" + day + "/" + year);
         
         System.out.println("Our school has really " + adj1 + " food.");
-        System.out.println("just thinking about it makes my stomach " + vb1 + ".");
+        System.out.println("Just thinking about it makes my stomach " + vb1 + ".");
         System.out.println("The spaghetti is " + adj2 + " and tastes like a " + noun1 + ".");
         System.out.println("One day, I swear one of my meatballs started to " + vb2 + "!");
-        System.out.println("The turkey tacos are totally " + adj3 + " and look kind of like old " + noun2 + ".");
+        System.out.println("The turkey tacos are totally " + adj3 + " and look kind of like an old " + noun2 + ".");
         System.out.println("My friend Dana actually likes the meatloaf even though it's " + adj4 + " and " + adj5 + ".");
         System.out.println("I call it mystery meatloaf and think it's really made out of " + noun3 + ".");
         System.out.println("My dad said he'd make my lunches, but the first day, he made me a sandwich out of " + noun4 + " and peanut butter!");
